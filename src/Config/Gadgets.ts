9aa2672aa7b7a9d49d745e53d8ab8c43fb305d08
Mehmet Emin Kartal <mehmet@appac.ltd>
2024-07-24 22:51:36 +0300

    Update Gadgets.ts

--- src/Config/Gadgets.ts
+++ src/Config/Gadgets.ts
@@ -1,13 +1,13 @@
 
-import RecoveryBootcode5Bin from '../../usbboot/firmware/2712/recovery.bin?url';
+// import RecoveryBootcode5Bin from '../../usbboot/firmware/2712/recovery.bin?url';
 import MassStorageGadgedBootFilesBin from '../../usbboot/firmware/bootfiles.bin?url';
-import BootImg from '../../usbboot/mass-storage-gadget64/boot.img?url';
+// import BootImg from '../../usbboot/mass-storage-gadget64/boot.img?url';
 import ConfigTxt from '../../usbboot/mass-storage-gadget64/config.txt?url';
-import RecoveryBootConf from '../../usbboot/recovery5/boot.conf?url';
-import PieepromBin from '../../usbboot/recovery5/pieeprom.bin?url';
-import PieepromSig from '../../usbboot/recovery5/pieeprom.sig?url';
-import RecoveryBin from '../../usbboot/recovery5/recovery.bin?url';
-import PieepromOriginalBin from '../../usbboot/rpi-eeprom/firmware-2712/default/pieeprom-2024-06-05.bin?url';
+// import RecoveryBootConf from '../../usbboot/recovery5/boot.conf?url';
+// import PieepromBin from '../../usbboot/recovery5/pieeprom.bin?url';
+// import PieepromSig from '../../usbboot/recovery5/pieeprom.sig?url';
+// import RecoveryBin from '../../usbboot/recovery5/recovery.bin?url';
+// import PieepromOriginalBin from '../../usbboot/rpi-eeprom/firmware-2712/default/pieeprom-2024-06-05.bin?url';
 
 export const availableGadgets = [
   {
@@ -16,7 +16,9 @@ export const availableGadgets = [
     bootfiles: MassStorageGadgedBootFilesBin,
     files: {
       'config.txt': ConfigTxt,
-      'boot.img': BootImg
+      // Cloudflare pages does not support files that are bigger than 25mb :(
+      // Todo: add hash checks to validate the file
+      'boot.img': "https://cdn.appac.dev/boot.img" //BootImg
     }
   },
   { // does not work yet
@@ -24,12 +26,12 @@ export const availableGadgets = [
     name: "Recovery EEPROM (Raspberry Pi 5)",
     bootfiles: MassStorageGadgedBootFilesBin,
     files: {
-      'boot.conf': RecoveryBootConf,
-      'bootcode5.bin': RecoveryBootcode5Bin,
-      'pieeprom.bin': PieepromBin,
-      'pieeprom.original.bin': PieepromOriginalBin,
-      'pieeprom.sig': PieepromSig,
-      'recovery.bin': RecoveryBin
+      // 'boot.conf': RecoveryBootConf,
+      // 'bootcode5.bin': RecoveryBootcode5Bin,
+      // 'pieeprom.bin': PieepromBin,
+      // 'pieeprom.original.bin': PieepromOriginalBin,
+      // 'pieeprom.sig': PieepromSig,
+      // 'recovery.bin': RecoveryBin
     }
   }
 ] as const;
